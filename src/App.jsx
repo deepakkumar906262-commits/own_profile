dbc2b4aab1005b6812658a0862ff5f9b597b0b4f
Deepak Kumar <deepakkumarbscc9062@gmail.com>
2026-08-05 20:36:07 +0530

    fix import path

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Navbar from "./components/Navbar/Navbar.jsx"; // .jsx laga kar check karein
+import Navbar from './components/Navbar/Navbar.jsx'; 
 import Hero from './components/Hero/Hero';
 import About from './components/About/About';
 import Education from './components/Education/Education';
